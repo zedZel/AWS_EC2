95fe3b644a5d9cd5a9380fe4e17a7f58eea1beda
zedZel <matt.e.zelenak@gmail.com>
2015-03-08 20:06:48 -0400

    Trying to get shelljs to laod properly so I can start writing bash commands from the grunt file

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,19 +1,17 @@
 /*global module:false*/
 module.exports = function(grunt) {
 
-    var shell = require("shelljs");
+    var shell = require('shelljs');
 
-    Project configuration.
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json');
+        pkg: grunt.file.readJSON('package.json')
     });
 
-    // Tasks
-    grunt.registerTask('getVolumes', function() {
-        // var allVolumes;
-        // shell.exec("aws ec2 describe-volumes > allVolumes.json");
-        // allVolumes = grunt.file.readJSON("allVolumes.json");
-        // console.log(allVolumes);
-        console.log("in getVolumes");
+    // Default task(s).
+    grunt.registerTask('default', function(){
+        var allVolumes;
+        shell.exec("aws ec2 describe-volumes > allVolumes.json");
+        allVolumes = grunt.file.readJSON("allVolumes.json");
+        console.log(allVolumes);
     });
 };
